d6e561d100116fe609dc1dcdefef62b08f7b56e3
Nita Youssef <nitayoussef3@gmail.com>
2023-10-06 15:22:23 -0700

    fifth check

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
-  * malloc_checked - allocates memory using malloc.
-  * @b: size of memory to be allocated.
-  *
-  * Return: pointer to allocated memory.
-  * exit with 98 if malloc fails 
-  */
+ * malloc_checked allocates the  memory using malloc
+ * 
+ * @b: The size of the memory to allocate
+ *
+ *
+ * Return: A pointer to the allocated memory
+ *
+ *         If malloc fails, the function  exit with  98.
+ */
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(b);
+    void *p = malloc(b);
 
-	if (p == NULL)
-		exit(98);
+    if (p == NULL)
+    {
+        exit(98);
+    }
 
-	return (p);
+    return (p);
 }
