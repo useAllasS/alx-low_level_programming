61b60f9990deeab21bfe2605003347ab8bef3258
Nita Youssef <nitayoussef3@gmail.com>
2023-10-19 15:25:21 -0700

    dd

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,56 +1,26 @@
 #include "lists.h"
-#include "_putchar.c"
+
 /**
- * print_list - Print elements in a linked list.
- * @h: Pointer to the head of the first node.
- *
- * Return: Number of nodes.
+ * print_list - print list
+ * @h : argument
+ * Return: Always 0.
  */
+
 size_t print_list(const list_t *h)
 {
-    const list_t *nav = h;
-    size_t count = 0;
+	const list_t *current = h;
+	int n = 0;
 
-    while (nav)
-    {
-        if (nav->str != NULL)
-        {
-            /* Print the length and the string */
-            unsigned int len = nav->len;
-            char *str = nav->str;
-            while (*str)
-            {
-                _putchar(*str);
-                str++;
-            }
-            _putchar('[');
-            while (len != 0)
-            {
-                int digit = len % 10;
-                _putchar('0' + digit);
-                len /= 10;
-            }
-            _putchar(']');
-            _putchar(' ');
-        }
-        else
-        {
-            /* If str is NULL, print (nil) */
-            _putchar('[');
-            _putchar('0');
-            _putchar(']');
-            _putchar(' ');
-            _putchar('(');
-            _putchar('n');
-            _putchar('i');
-            _putchar('l');
-            _putchar(')');
-        }
-
-        _putchar('\n');  
-        count++;
-        nav = nav->next;
-    }
-
-    return (count);
+	while (current != NULL)
+	{
+		if (!current->str)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%d] %s\n", current->len, current->str);
+		}
+		n++;
+		current = current->next;
+	}
+	return (n);
 }
